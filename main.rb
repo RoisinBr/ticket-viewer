f6ab28c7365b1ecdbe70f9113621836ff092fa56
Roisin <roisin.briscoe@gmail.com>
2019-06-08 11:19:57 +1000

    api connection made with password env, will need to remove for submission

--- main.rb
+++ main.rb
@@ -7,6 +7,13 @@ get '/' do
   erb :index
 end
 
+get '/ticket-list' do
+  authentication = {:username => "roisin.briscoe@gmail.com", :password => ENV["zendesk_password"]}
+  url = 'https://roisin.zendesk.com/api/v2/tickets.json'
+  @tickets = HTTParty.get(url, :basic_auth => authentication)
+  erb :list_tickets
+end
+
 
 
 
