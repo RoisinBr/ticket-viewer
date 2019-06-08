a7d7b8f19de6147e3ea9c3063623aae079183027
Roisin <roisin.briscoe@gmail.com>
2019-06-08 13:29:45 +1000

    list now shows 25 but doesn't have next page capability yet

--- main.rb
+++ main.rb
@@ -9,10 +9,23 @@ end
 
 get '/ticket-list' do
   authentication = {:username => "roisin.briscoe@gmail.com", :password => ENV["zendesk_password"]}
-  url = 'https://roisin.zendesk.com/api/v2/tickets.json'
+  url = 'https://roisin.zendesk.com/api/v2/tickets.json?per_page=25'
   @tickets = HTTParty.get(url, :basic_auth => authentication)
   erb :list_tickets
 end
+
+# get '/ticket-list/next-page' do
+#   authentication = {:username => "roisin.briscoe@gmail.com", :password => ENV["zendesk_password"]}
+#   if url
+#     url = "#{params["link"]}"
+#     @tickets = HTTParty.get(url, :basic_auth => authentication)
+#     erb :list_tickets
+#   end
+# end
+
+# while url do
+#   url = @tickets["next_page"]
+# end 
 
 
 
